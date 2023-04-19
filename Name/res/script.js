564221bfe466beecc06b2ece70cd03a62da71758
G-xming <GitHub@emailed.cf>
2023-04-19 09:00:36 -0500

    t

--- Name/res/script.js
+++ Name/res/script.js
@@ -2,7 +2,7 @@ function one() {
 	const Original = document.location.search
 	const Edited = Original.slice(1)
 	const FName = Edited.split('FName=')[1];
-	const FName2 = FName.split('~')[0];
+	const FName2 = FName.split('&')[0];
 	document.getElementById('x').innerHTML = "First Name: " + FName2
 	const LName = Edited.split('LName=')[1];
 	document.getElementById('y').innerHTML = "Last Name: " + LName
